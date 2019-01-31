eeb94cfa5550026d68e135f541ae75c8f3ff58fb
Mickey <micksheridan.24@gmail.com>
2019-01-31 13:49:54 -0500

    submit

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,21 @@
 def alphabetize(arr)
   # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+
+
+  arr = arr.sort do |a, b|
+    
+    a_f = alphabet.index(a[0])
+    b_f = alphabet.index(b[0])
+    c = 1
+    while (a_f == b_f)
+      a_f = alphabet.index(a[c])
+      b_f = alphabet.index(b[c])
+      c += 1
+    end
+    
+    a_f<=>b_f
+
+  end
 end
